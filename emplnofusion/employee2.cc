110513e7f378476dd2dc15bc54485d9730f56505
Jorge Ventura <jorge.araujo.ventura@gmail.com>
2022-03-25 18:28:10 -0500

    More tests

---
+++ emplnofusion/employee2.cc
@@ -0,0 +1,108 @@
+#define BOOST_SPIRIT_X3_DEBUG
+#include <iostream>
+#include <boost/spirit/home/x3.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <string>
+#include <vector>
+#include <variant>
+
+namespace x3 = boost::spirit::x3;
+
+namespace ast {
+   struct employee
+   {
+       int age;
+       std::string gname;
+       std::string sname;
+       float sal;
+
+       employee() : age(0), sal(0.0) {}
+
+       friend std::ostream& operator<<(std::ostream& os, employee const& e)
+       {
+	   os << "[ " << e.age << ", \"" << e.gname << "\", \"" << e.sname << "\", " << float(e.sal) << " ]" << std::endl;
+           return os;
+       }
+
+
+   };
+       
+}
+
+BOOST_FUSION_ADAPT_STRUCT(ast::employee, age, gname, sname, sal);
+
+
+namespace parser {
+    namespace x3 = boost::spirit::x3;
+
+    x3::rule<struct employee_class, ast::employee> const employee_ = "employee";
+
+    x3::real_parser<float, x3::strict_real_policies<float> > float_;
+
+    auto const quoted_string = x3::lexeme['"' >> +(x3::char_ - '"') >> '"'];
+
+    const auto employee__def =
+            x3::lit("employee")
+            >> '{'
+            >>  (x3::int_ >> ',')
+            >>  (quoted_string >> ',')
+            >>  (quoted_string >> ',')
+            >>  float_
+            >>  '}'
+            ;
+
+    BOOST_SPIRIT_DEFINE(employee_)
+    
+    const auto entry_point = x3::skip(x3::space) [ employee_ ];
+    
+
+}
+
+int main()
+{
+    namespace x3 = boost::spirit::x3;
+
+    std::cout << "/////////////////////////////////////////////////////////\n\n";
+    std::cout << "\t\tAn employee parser for Spirit...\n\n";
+    std::cout << "/////////////////////////////////////////////////////////\n\n";
+
+    std::cout
+        << "Give me an employee of the form :"
+        << "employee{age, \"forename\", \"surname\", salary } \n";
+    std::cout << "Type [q or Q] to quit\n\n";
+
+    using iterator_type = std::string::const_iterator;
+
+    std::string str;
+    while (getline(std::cin, str))
+    {
+        if (str.empty() || str[0] == 'q' || str[0] == 'Q')
+            break;
+
+        ast::employee emp;
+
+        iterator_type iter = str.begin();
+        iterator_type const end = str.end();
+
+        bool r = phrase_parse(iter, end, parser::entry_point, x3::space, emp);
+
+        if (r && iter == end)
+        {
+            std::cout << "-------------------------\n";
+            std::cout << "Parsing succeeded\n";
+            std::cout << "got: " << emp << std::endl;
+            std::cout << "\n-------------------------\n";
+        }
+        else
+        {
+            std::cout << "-------------------------\n";
+            std::cout << "Parsing failed\n";
+            std::cout << "-------------------------\n";
+        }
+    }
+
+    std::cout << "Bye... :-) \n\n";
+    return 0;
+}
+
+
--- emplnofusion/employee3.cc
+++ emplnofusion/employee3.cc
@@ -1,59 +1,54 @@
 #define BOOST_SPIRIT_X3_DEBUG
 #include <iostream>
 #include <boost/spirit/home/x3.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 #include <string>
 #include <vector>
 #include <variant>
 
+namespace x3 = boost::spirit::x3;
+
 namespace ast {
    struct employee
    {
-       int age;
-       std::string gname;
-       std::string sname;
-       float sal;
+       std::vector<int> age;
 
-       employee() : age(0), sal(0.0) {}
+       employee() {}
 
        friend std::ostream& operator<<(std::ostream& os, employee const& e)
        {
-	   os << "[ " << e.age << ", \"" << e.gname << "\", \"" << e.sname << "\", " << float(e.sal) << " ]" << std::endl;
+	   os << "[ ";
+	   for (const int& i : e.age) {
+	       os << i << ", ";
+	   }
+	   os << " ]" << std::endl;
            return os;
        }
 
 
    };
+       
 }
+
+BOOST_FUSION_ADAPT_STRUCT(ast::employee, age);
+
 
 namespace parser {
     namespace x3 = boost::spirit::x3;
 
     x3::rule<struct employee_class, ast::employee> const employee_ = "employee";
 
-    x3::real_parser<float, x3::strict_real_policies<float> > float_;
-
-    auto const quoted_string = x3::lexeme['"' >> +(x3::char_ - '"') >> '"'];
-
-    auto assign_age   = [](auto ctx) { _val(ctx).age = _attr(ctx); };
-    auto assign_gname = [](auto ctx) { _val(ctx).gname = _attr(ctx); };
-    auto assign_sname = [](auto ctx) { _val(ctx).sname = _attr(ctx); };
-    auto assign_sal   = [](auto ctx) { _val(ctx).sal   = _attr(ctx); };
-    //auto assign_sal   = [](auto ctx) { std::cout << _attr(ctx) << std::endl; };
-
     const auto employee__def =
             x3::lit("employee")
             >> '{'
-            >>  (x3::int_ >> ',')[assign_age]
-            >>  (quoted_string >> ',')[assign_gname]
-            >>  (quoted_string >> ',')[assign_sname]
-            >>  float_[assign_sal]
-            >>  '}'
+            >>  x3::int_ % x3::char_(',')
+            >> '}'
             ;
-
 
     BOOST_SPIRIT_DEFINE(employee_)
     
     const auto entry_point = x3::skip(x3::space) [ employee_ ];
+    
 
 }
 
@@ -87,14 +82,9 @@ int main()
 
         if (r && iter == end)
         {
-
-            //std::cout << boost::fusion::tuple_open('[');
-            //std::cout << boost::fusion::tuple_close(']');
-            //std::cout << boost::fusion::tuple_delimiter(", ");
-
             std::cout << "-------------------------\n";
             std::cout << "Parsing succeeded\n";
-            //std::cout << "got: " << emp << std::endl;
+            std::cout << "got: " << emp << std::endl;
             std::cout << "\n-------------------------\n";
         }
         else
